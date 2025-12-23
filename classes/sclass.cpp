8047434d91c1d4b01cf88b584f2fdf8a181ef133
SAHOWI <wienecke.ch@gmail.com>
2025-12-23 17:36:31 +0100

    Added simple class methods

--- classes/sclass.cpp
+++ classes/sclass.cpp
@@ -8,16 +8,45 @@ class MyClass {
 		 int zzz = 0;
 	public:
 		int xxx = 0;
-		// print(my_class myc);
-		// add(my_class myc);
-		// sub(my_class myc);
+		int print();
+		int add();
+		int add(int);
+		int sub();
 	
 };
+
+int MyClass::print()
+{
+	cout << zzz << "\n";
+	return(0);
+}
+
+int MyClass::add()
+{
+	zzz++;
+	return(0);
+}
+
+int MyClass::add(int inc)
+{
+	zzz = zzz + inc;
+	return(0);
+}
+
+
+int MyClass::sub()
+{
+	zzz--;
+	return(0);
+}
 
 
 int main ()
 {
-	MyClass my_class; 
+	MyClass my_class; // internal counter is now 0
+	my_class.add();   // internal counter is now 1
+	my_class.add(5);  // internal counter is now 6
+	my_class.print();
 
 	return (0);
 }

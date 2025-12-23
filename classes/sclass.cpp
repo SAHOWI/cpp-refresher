4d5ca30919fbcbe23d0cac7f7cd9f0299837d2da
SAHOWI <wienecke.ch@gmail.com>
2025-12-23 17:21:53 +0100

    Initial Class Test

---
+++ classes/sclass.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+class MyClass {
+	private:
+		 int zzz = 0;
+	public:
+		int xxx = 0;
+		// print(my_class myc);
+		// add(my_class myc);
+		// sub(my_class myc);
+	
+};
+
+
+int main ()
+{
+	MyClass my_class; 
+
+	return (0);
+}
